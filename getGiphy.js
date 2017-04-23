40705ba6985c106f63591f7919deda2495e6ad47
JCR08 <j.c.rowell15@gmail.com>
2017-04-23 13:19:58 -0400

    commented out jamm giphy part

--- getGiphy.js
+++ getGiphy.js
@@ -8,12 +8,12 @@ function getGiphy(){
 
 getGiphy()
 
-function jammGiphy(){
- fetch("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=jeremy+jamm")
- .then(response => response.json())
- .then(object => {
- document.getElementById("jammed").src=object.data.image_url;
- });
-}
-
-jammGiphy()
+// function jammGiphy(){
+//  fetch("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=jeremy+jamm")
+//  .then(response => response.json())
+//  .then(object => {
+//  document.getElementById("jammed").src=object.data.image_url;
+//  });
+// }
+//
+// jammGiphy()
